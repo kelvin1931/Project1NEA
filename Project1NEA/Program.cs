ab07cac0ae292805a2c77813506c7ce8161d5d1f
Kelvin James <14JamesK@student.smchull.org>
2026-01-09 15:31:29 +0000

    Proj1Dev

--- Project1NEA/Program.cs
+++ Project1NEA/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Mathematics;
+
+namespace BuildObject
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string name) : base(GameWindowSettings.Default, new NativeWindowSettings()
+        {
+            Size = new Vector2i(width, height),
+            Title = name
+        })
+        { }
+        
+    }
+}
