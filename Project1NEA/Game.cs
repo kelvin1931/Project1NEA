c24fbe9cf827ff4778b29856ace3c8713588ce60
Kelvin James <14JamesK@student.smchull.org>
2026-01-14 11:19:22 +0000

    FinallyDevelopedTriangle

--- Project1NEA/Game.cs
+++ Project1NEA/Game.cs
@@ -2,29 +2,36 @@
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using Project1NEA;
+using static Project1NEA.Shaders;
 
-namespace project1
+namespace Project1NEA
 {
     public class Game :GameWindow
     {
+        private Shader shader;
         int VertexBufferObject;
+        int VertexArrayObject;
         float[] vertices =
             {
             -0.5f, -0.5f, 0.0f, //Bottom-left vertex
              0.5f, -0.5f, 0.0f, //Bottom-right vertex
              0.0f,  0.5f, 0.0f  //Top vertex
             };
+        
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
         { }
 
         public static void Main(string[] args)
         {
-            using (Game game = new Game(800, 600, "GameWindow")) //1440,1080
+            using (Game game = new Game(1440, 1080, "GameWindow")) //1440,1080
             {
                 game.Run();
             }
         }
-            protected override void OnUpdateFrame(FrameEventArgs args)
+
+        #region UpdateFrame
+        protected override void OnUpdateFrame(FrameEventArgs args)
         {
             base.OnUpdateFrame(args);
             if (KeyboardState.IsKeyDown(Keys.Escape))
@@ -32,30 +39,65 @@ namespace project1
                 Close();
             }
         }
+        #endregion
 
-       protected override void OnLoad()
+        #region OnLoad
+        protected override void OnLoad()
         {
             base.OnLoad();
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+            shader = new Shader("shader.vert", "Rshader.frag");
+
+            // VAO
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            // VBO
             VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer,vertices.Length * sizeof(float),vertices,BufferUsageHint.StaticDraw);
+
+            // Link vertex attributes
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+
+
+            //someOpenGLFunctionThatDrawsOurTriangle();
         }
-        protected override void OnRenderFrame(FrameEventArgs e)
+
+    #endregion
+
+    #region RenderFrame
+    protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+
+            GL.UseProgram(shader.Handle);
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
             SwapBuffers();
         }
+        #endregion
+
+        #region FrameBuffer
         protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
         {
             base.OnFramebufferResize(e);
             GL.Viewport(0, 0, e.Width, e.Height);
             GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-        }
 
+ 
+        }
+        #endregion
+
+        
     }
 
 }
